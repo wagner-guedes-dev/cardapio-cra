2e5bd86a91602d1d7252ef695ad4b05c7d9f58c5
Wagner Guedes <wagnerguedes123@hotmail.com>
2024-04-23 14:11:11 -0300

    Update routes.jsx

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route} from 'react-router-dom'
 import Container from './pages/Container'
 import CentralPedidos from './pages/CentralPedidos'
+import SpaceIce from './pages/SpaceIce'
 
 
 function RoutesApp(){
@@ -9,6 +10,7 @@ function RoutesApp(){
             <Routes>
                 <Route path='/' element={ <Container/> }/>
                 <Route path='/admin' element={ <CentralPedidos/> }/>
+                <Route path='space-ice' element={ <SpaceIce/> } />
             </Routes>
         </BrowserRouter>
     )
